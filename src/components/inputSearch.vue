6625b28af37490d5e53f23c4f3bf7dcfeaf7a108
thinhnguyen1101 <thinhb1910709@student.ctu.edu.vn>
2022-10-28 23:53:10 +0700

    Create a contact listing page

---
+++ src/components/inputSearch.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+    prop: {
+        moduleValue: {  type: String, default: ''},
+    },
+    emits: ['submit', 'update:moduleValue'],
+    methods: {
+        updateModelValue(e) {
+            this.$emit('update:moduleValue', e.target.value);
+        },
+        submit() {
+            this.$emit('submit');
+        },
+    },
+};
+</script>
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="Nhập thông tin cần tìm" :value="modelValue"
+            @input="updateModelValue" @keyup.enter="submit">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" @click="submit">
+                <i class="fas fa-search" /> Tìm kiếm
+            </button>
+        </div>
+    </div>
+</template>
